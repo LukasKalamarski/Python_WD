a0e1be4feaf027cd65191248ffe1496c99729201
LukasKalamarski <58850197+LukasKalamarski@users.noreply.github.com>
2020-03-11 11:04:03 +0100

    Add files via upload

--- python.py
+++ python.py
@@ -1,2 +1,54 @@
-imie = "Jorgen"
-print(imie[::1])
+# 1
+# zdanie = input("napisz coś ")
+# print(zdanie.count(" "))
+
+# 2
+# a = input()
+# b = input()
+# c = int(a) + int(b)
+# print(c)
+
+# 4
+# a = int(input())
+# if a >= 0 :
+#     print(a)
+# else:
+#     print(a * -1)
+
+# 5 
+# a = int(input())
+# b = int(input()) 
+# c = int(input()) 
+# if ( a >= 0 and a <= 10 and a > b or b > c):
+#     print("OK")
+# else:
+#     print("Not OK")
+
+# timeit
+
+# {indeks:litera for indeks, litera in enumerate('Wojtek')}
+
+# slownk ={'imie': 'Marian', 'nazwisko': 'Kowalski'}
+
+# def dodaj(liczba1, liczba2):
+#     print(liczba1 + liczba2)
+
+# dodaj(1, 2)
+
+# def witaj(imie='Jan', nazwisko='Kowalski'):
+#     print(f"witaj {imie} {nazwisko}!")
+# witaj()
+
+# def witaj(imie='Jan', nazwisko='Kowalski'):
+#     print(f"witaj {imie} {nazwisko}!")
+
+# witaj(nazwisko ='Malinowski')
+
+# [print(1 / x) for x in range(1,11)]
+
+# A = [x**2 for x in range(1,11)]
+# print(A)
+
+B = [[x = random.radinit(1, 100) for x in range(1,5)], [x = random.radinit(1, 100) for x in range(1,5)], [x = random.radinit(1, 100) for x in range(1,5)], [x = random.radinit(1, 100) for x in range(1,5)]
+print(B)]
+
